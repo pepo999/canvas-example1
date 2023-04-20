c6fd0bd1b1f553d709b9d05112d597fa4a2a8777
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-20 18:57:43 +0200

    update

--- app.js
+++ app.js
@@ -2,54 +2,51 @@ const myCanvas = document.getElementById('my-canvas');
 
 const ctx = myCanvas.getContext('2d');
 
-const point = {
-    x: 400,
-    y: 400
-}
+const inputX = document.getElementById('x-input');
+const inputY = document.getElementById('y-input');
+let ctxX = inputX.value;
+let ctxY = inputX.value;
+myCanvas.width = ctxX;
+myCanvas.height = ctxY;
 
-let r = 120;
-let g = 1;
+let r = 130;
+let g = 110;
 let b = 120;
 
-ctx.filter = "blur(1px)";
+ctx.filter = "blur(1.5px)";
 
-for (let i = 0; i < 1000; i++) {   
-    setInterval(() => {
-        if(point.x > 800) {point.x = 400}
-        if(point.y > 800) {point.y = 400}
-        if(point.x < 0) {point.x = 400}
-        if(point.y < 0) {point.y = 400}
-        r+= Math.random() - Math.random();
-        g+=Math.random() - Math.random();
-        b+=Math.random() - Math.random();
-        ctx.fillStyle = `rgb(${r},${g},${b})`;
-        ctx.fillRect(point.x, point.y, 1, 1);
-        point.x = point.x + 2* Math.random() - 2* Math.random();
-        point.y = point.y + 2* Math.random() - 2* Math.random(); 
-    }, 100);
+
+function walk(point, interval) {
+    for (let i = 0; i < 1000; i++) {  
+        setInterval(() => {
+            if(point.x > ctxX) {point.x = 0}
+            if(point.y > ctxY) {point.y = 0}
+            if(point.x < 0) {point.x += ctxX-1}
+            if(point.y < 0) {point.y += ctxY-1}
+    
+            r = r + Math.random() - Math.random();
+            g = g + Math.random() - Math.random();
+            b = b + Math.random() - Math.random();
+            ctx.fillStyle = `rgb(${r},${g},${b},1)`;
+            ctx.fillRect(point.x, point.y, (Math.random()) + 2, (Math.random()) + 2);
+            point.x = point.x + (Math.random() - Math.random())* 2;
+            point.y = point.y + (Math.random() - Math.random())* 2; 
+        }, interval);
+    }
 }
 
-// const point2 = {
-//     x: 400,
-//     y: 400
-// }
+const point1 = new Point()
+const point2 = new Point()
+const point3 = new Point()
+const point4 = new Point()
 
+// darken(1)
+// walk(point1, 25)
+let blur = 0;
 
-// ctx.filter = "blur(1px)";
-
-// for (let i = 0; i < 1000; i++) {   
-//     setInterval(() => {
-//         if(point2.x > 800) {point2.x = 400}
-//         if(point2.y > 800) {point2.y = 400}
-//         if(point2.x < 0) {point2.x = 400}
-//         if(point2.y < 0) {point2.y = 400}
-//         r+= Math.random() - Math.random();
-//         g+=Math.random() - Math.random();
-//         b+=Math.random() - Math.random();
-//         ctx.fillStyle = `rgb(${r},${g},${b})`;
-//         ctx.fillRect(point2.x, point2.y, 1, 1);
-//         point2.x = point2.x + 2* Math.random() - 2* Math.random() - 0.5;
-//         point2.y = point2.y + 2* Math.random() - 2* Math.random() - 0.5; 
-//     }, 100);
-// }
-
+function darken(interval){
+    setInterval(() => {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'; 
+        ctx.fillRect(0, 0, ctxX, ctxY);
+    }, interval);
+}
---
+++ point.js
@@ -0,0 +1,8 @@
+class Point {
+
+    constructor() {
+        this.x = Math.random() * 800;
+        this.y = Math.random() * 800;
+    }
+
+}
