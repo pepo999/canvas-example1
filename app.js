97b1ea3261864faafe0e8b97b57df02bf11c9d93
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-20 11:13:46 +0200

    working

--- app.js
+++ app.js
@@ -12,80 +12,86 @@ const ctx = myCanvas.getContext('2d');
 //     ctx.strokeRect(originX, originy, width, height);
 // }
 
-let maxIterationsInput = document.getElementById('iteration-input')
-const iterations = maxIterationsInput.value;
-
+let inputX;
+let xValue;
+let maxIterationsInput;
+let iterations;
 let r = 190;
-let g = 1;
+let g = 0;
 let b = 190;
 
-
 function generate() {
-    ctx.fillRect(0, 0, 1000, 1000);
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    let startX = 400;
-    let startY = 400;
+    inputX = document.getElementById('x-input');
+    xValue = inputX.value;
+    maxIterationsInput = document.getElementById('iteration-input');
+    iterations = maxIterationsInput.value;
+    myCanvas.width = xValue;
+    myCanvas.height = xValue;
+    ctx.fillStyle = `rgb(0, 0, 0)`;
+    ctx.fillRect(0, 0, xValue, xValue);
+    let startX = xValue / 2;
+    let startY = xValue / 2;
     let counter = 1;
     ctx.strokeStyle = `rgb(${r},${g},${b})`;
     ctx.beginPath();
-    ctx.moveTo(400, 800);
+    ctx.moveTo(xValue / 2, xValue);
     ctx.lineTo(startX, startY);
     ctx.stroke();
-    // recursiveRight(startX, startY, counter);
-    // recursiveLeft(startX, startY, counter);
     recursive(startX, startY, counter)
 }
 
-function recursive(startx, starty, counter) {
-    console.log('startX = ' + startx + ' , startY = ' + starty + ' and counter = ' + counter)
-    if(counter * counter >= iterations) {
+function recursive(startX, startY, counter) {
+    console.log('startX = ' + startX + ' , startY = ' + startY + ' and counter = ' + counter)
+    if (counter * counter >= iterations) {
         return;
     }
     ctx.strokeStyle = `rgb(${r},${g},${b})`
     ctx.beginPath();
-    ctx.moveTo(startx, starty);
-    let newStartXl = startx - (starty / 2);
-    let newStartYl = starty / 2;
+    ctx.moveTo(startX, startY);
+    let newStartXl = startX - (startY / 2);
+    let newStartYl = startY / 2;
     ctx.lineTo(newStartXl, newStartYl);
     ctx.stroke();
-    ctx.moveTo(startx, starty);
-    let newStartXr = startx + (starty / 2);
-    let newStartYr = starty / 2;
+    ctx.moveTo(startX, startY);
+    let newStartXr = startX + (startY / 2);
+    let newStartYr = startY / 2;
     ctx.lineTo(newStartXr, newStartYr);
     ctx.stroke();
-    recursive(newStartXr, newStartYr, counter + 1);
-    recursive(newStartXl, newStartYl, counter + 1);
+    counter++;
+    recursive(newStartXr, newStartYr, counter);
+    recursive(newStartXl, newStartYl, counter);
 }
 
-// function recursiveLeft(startx, starty, counter) {
-//     console.log('startXL = ' + startx + ' , startyL = ' + starty + ' and counterL = ' + counter)
-//     if ((counter * counter) >= iterations) {
-//         return;
-//     }
-//     ctx.beginPath();
-//     ctx.moveTo(startx, starty);
-//     let newStartX = startx - (starty / 2);
-//     let newStartY = starty / 2;
-//     ctx.lineTo(newStartX, newStartY);
-//     ctx.stroke();
-//     recursiveLeft(newStartY, newStartY, counter + 1);
-//     recursiveRight(newStartX, newStartY, counter + 1);
-
-// }
-
-// function recursiveRight(startx, starty, counter) {
-//     console.log('startXR = ' + startx + ' , startyR = ' + starty + ' and counterR = ' + counter)
-//     if ((counter * counter) >= iterations) {
+// function recursive(startX, startY, counter) {
+//     console.log('startX = ' + startX + ' , startY = ' + startY + ' and counter = ' + counter)
+//     if (counter * counter >= iterations) {
 //         return;
 //     }
 //     ctx.strokeStyle = `rgb(${r},${g},${b})`
 //     ctx.beginPath();
-//     ctx.moveTo(startx, starty);
-//     let newStartX = startx + (starty / 2);
-//     let newStartY = starty / 2;
-//     ctx.lineTo(newStartX, newStartY);
+//     ctx.moveTo(startX, startY);
+//     let newStartXlUp = startX - (startY / 2);
+//     let newStartYlUp = startY / 2;
+//     ctx.lineTo(newStartXlUp, newStartYlUp);
 //     ctx.stroke();
-//     recursiveLeft(newStartX, newStartY, counter + 1);
-//     recursiveRight(newStartX, newStartY, counter + 1);
+//     ctx.moveTo(startX, startY);
+//     let newStartXrUp = startX + (startY / 2);
+//     let newStartYrUp = startY / 2;
+//     ctx.lineTo(newStartXrUp, newStartYrUp);
+//     ctx.stroke();
+//     ctx.moveTo(startX, startY);
+//     let newStartXlDown = startY /2;
+//     let newStartYlDown = startX + (startY / 2);
+//     ctx.lineTo(newStartXlDown, newStartYlDown);
+//     ctx.stroke();
+//     ctx.moveTo(startX, startY);
+//     let newStartXrDown = startY /2;
+//     let newStartYrDown = startX + (startY / 2);
+//     ctx.lineTo(newStartXrDown, newStartYrDown);
+//     ctx.stroke();
+//     counter++;
+//     recursive(newStartXrUp, newStartYrUp, counter);
+//     recursive(newStartXlUp, newStartYlUp, counter);
+//     recursive(newStartXrDown, newStartYrDown, counter);
+//     recursive(newStartXlDown, newStartYlDown, counter);
 // }
-
