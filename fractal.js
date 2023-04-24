45a395ed7d361fda0922102e5c78bbf7864d70a2
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-24 11:37:43 +0200

    Update fractal.js

--- fractal.js
+++ fractal.js
@@ -3,15 +3,6 @@ const myCanvas = document.getElementById('my-canvas');
 
 const ctx = myCanvas.getContext('2d');
 
-// for (let i = 0; i < 100; i++) {
-//     const originX = Math.random() * 600;
-//     const originy = Math.random() * 600;
-//     const width = Math.random() * 100;
-//     const height = Math.random() * 100;
-//     ctx.fillStyle = Math.round(Math.random() * 15)
-//     ctx.strokeRect(originX, originy, width, height);
-// }
-
 let inputX;
 let xValue;
 let maxIterationsInput;
@@ -33,10 +24,6 @@ function generate() {
     let startY = xValue / 2;
     let counter = 1;
     ctx.strokeStyle = `rgb(${r},${g},${b})`;
-    // ctx.beginPath();
-    // ctx.moveTo(xValue / 2, xValue);
-    // ctx.lineTo(startX, startY);
-    // ctx.stroke();
     recursive(startX, startY, counter);
     ctx.translate(xValue, xValue)
     ctx.rotate(Math.PI);
